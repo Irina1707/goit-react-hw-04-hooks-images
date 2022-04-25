04309f0df6062640c300d248db541a8e18cfcf62
Irina1707 <irina_niv@mail.ru>
2022-04-25 23:43:59 +0300

    change image-gallery

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -7,10 +7,17 @@ import { Loader } from '../Loader/Loader';
 import Modal from '../Modal/Modal'
 import { ImageGalleryStyle } from './ImageGallery.styled';
 
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+}
+
 export default function ImageGallery({ searchQuery }) {
 
   const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState(Status.IDLE);
   const [currentPage, setCurrentPage] = useState(1);
   const [error, setError] = useState(null);
   const [largeImageURL, setLargeImageURL] = useState('');
@@ -28,7 +35,7 @@ export default function ImageGallery({ searchQuery }) {
      
     if (searchQuery !== prevName) {
       setCurrentPage(1)
-      setLoading(true)
+      setStatus(Status.PENDING)
       setImages([]) 
       setPrevName(searchQuery)       
       }
@@ -44,23 +51,24 @@ export default function ImageGallery({ searchQuery }) {
  
   // const handleLoadMore = () => {
   //   scroll();
-  //   setLoading(true);
+  //   setStatus(Status.PENDING);
   //   setCurrentPage(currentPage + 1); 
   //   }
 
  const fetchImages = () => {
-   setLoading(true);
-   
-      API.fetchImages(searchQuery, currentPage)
-        .then((data) => {
-          setImages(prevImages => [...prevImages, ...data.hits]);
-          setCurrentPage((prevPage) => prevPage + 1);
-        })
-      .catch(error => setError(error))
-      .finally(() => {
-        setLoading(false);
-        scroll();
-      });
+   setStatus(Status.PENDING);
+   API.fetchImages(searchQuery, currentPage)
+     .then((data) => {
+       setImages(prevImages => [...prevImages, ...data.hits]);
+       setCurrentPage((prevPage) => prevPage + 1);
+       setStatus(Status.RESOLVED)
+       setError(null)
+     })
+     .catch(error => {
+       setError(error);
+       setStatus(Status.REJECTED);
+     })
+     .finally(() => scroll());
   }
 
   const handleLargeImage = (image) => {
@@ -71,28 +79,27 @@ export default function ImageGallery({ searchQuery }) {
     setShowModal(false);
   }
  
-  const shouldRenderLoadMoreButton = images.length > 0 && !loading;
-  
-    return (
-      <div>
-        {error && <p>{error.message}</p>}
-    
-        {loading && <Loader />}
-            <ToastContainer autoClose={3000} />
-            
-        {images.length > 0 && <ImageGalleryStyle>
-            {images.map(({ id, webformatURL, largeImageURL}) => (
-                <ImageGalleryItem
-                    key={id}
-                    webformatURL={webformatURL}
-                    largeImageURL={largeImageURL}
-                    alt={searchQuery}
-                    onClick={handleLargeImage} />))}
-                </ImageGalleryStyle>}
-
-        {showModal && <Modal largeImageURL={largeImageURL} onClose={closeModal}/>}
-        {shouldRenderLoadMoreButton  && <Button onClick={fetchImages} />}
-       </div>     
-            );
+  return (
+      <>
+  {status === Status.REJECTED && <p>{error.message}</p>}
+  {status === Status.PENDING && <Loader />} 
+  <ToastContainer autoClose={3000} />
+  {status === Status.RESOLVED && (
+    <>
+    <ImageGalleryStyle>
+      {images && images.map(({ id, webformatURL, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          alt={searchQuery}
+          onClick={handleLargeImage} />))}
+      </ImageGalleryStyle>;
+    </>)}
+  {showModal && <Modal largeImageURL={largeImageURL} onClose={closeModal} />}
+  {images && status === Status.RESOLVED && <Button onClick={fetchImages} />}
+      </>
+      )
+                     
 }
 
